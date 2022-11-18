0d320b8473bd09db5a43f6cab0735c690dcde70e
Luis Henrique <luique16@outlook.com>
2022-11-18 17:52:26 -0400

    first commit

---
+++ band_scoe/src/Login.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import "./Login.css";
+
+function Login() {
+  const [count, setCount] = useState(0);
+
+  return (
+    
+      <div className="Card-login">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        
+      </div>
+    
+  );
+}
+
+export default Login;
