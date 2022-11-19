f97bcda677fe90aa5228d16fea962c914a190542
Luis Henrique <luique16@outlook.com>
2022-11-19 11:40:19 -0400

    componente login mdify

--- band_scoe/src/Login.jsx
+++ band_scoe/src/Login.jsx
@@ -5,15 +5,36 @@ function Login() {
   const [count, setCount] = useState(0);
 
   return (
-    
-      <div className="Card-login Item-login" >
+    <div className="container-centralizador">
+      <div className="Card-login Item-login">
         <span></span>
         <span></span>
         <span></span>
         <span></span>
-        
+
+        <div className="title">
+          <h2>Login</h2>
+        </div>
+<div className="line">
+
+</div>
+        <div >
+
+          <div className="inputs">
+            <label>Nome: </label>
+            <input type="text" />
+          </div>
+
+          <div className="inputs" >
+            <label>Senha: </label>
+            <input type="text" />
+          </div>
+
+
+        </div>
+
       </div>
-    
+    </div>
   );
 }
 
