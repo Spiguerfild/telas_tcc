9691a37afb6eb0ec09722ad660f8a6cd5cf74042
Luis Henrique <luique16@outlook.com>
2022-11-25 17:58:53 -0400

    card escala

--- src/BlocoEscala.jsx
+++ src/BlocoEscala.jsx
@@ -1,16 +1,24 @@
 import "./BlocoEscala.css";
 
+import band_ft from "./assets/Band-Image.png";
 function BlocoEscala() {
   return (
     <div className="container">
       <div className="block">
         <header className="top-block">
-          <h3>Nome da banda</h3>
+          <h3>Nome da Escala</h3>
         </header>
-        <div className="corpo">
-          <div className="lado-direito"></div>
+
+        <div className="bottom-block">
           <div className="lado-esquerdo">
-           <img src="" alt="" />
+            <img className="foto-esquerda" src={band_ft} width={200} />
+          </div>
+
+          <div className="lado-direito">
+            <h5>Data:  27/11/2022</h5>
+            <h5>Local: Igreja</h5>
+            <h5>Horario: 17:30</h5>
+            
           </div>
         </div>
       </div>
