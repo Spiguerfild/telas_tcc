e17665837569903d8bd5a8826db825abf3b40528
Luis Henrique <luique16@outlook.com>
2022-11-25 14:04:50 -0400

    d

---
+++ src/BlocoEscala.jsx
@@ -0,0 +1,21 @@
+import "./BlocoEscala.css";
+
+function BlocoEscala() {
+  return (
+    <div className="container">
+      <div className="block">
+        <header className="top-block">
+          <h3>Nome da banda</h3>
+        </header>
+        <div className="corpo">
+          <div className="lado-direito"></div>
+          <div className="lado-esquerdo">
+           <img src="" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default BlocoEscala;
