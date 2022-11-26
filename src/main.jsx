3fc511074aaf75cb05bc295080013cf824695b29
Luis Henrique <luique16@outlook.com>
2022-11-25 20:43:19 -0400

    .

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Login from './Login'
 import './index.css'
+
 import BlocoEscala from './BlocoEscala'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BlocoEscala />
-<Login/>
+<BlocoEscala /><BlocoEscala />
+
   </React.StrictMode>
 )
