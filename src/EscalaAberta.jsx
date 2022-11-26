30d1fd665edc943289bcc7a35215ef94452fac11
Luis Henrique <luique16@outlook.com>
2022-11-26 12:26:12 -0400

    escala_aberta

--- src/EscalaAberta.jsx
+++ src/EscalaAberta.jsx
@@ -1,30 +1,44 @@
-import './EscalaAberta.css'
-import MusicoInstrumento from './MusicoInstrumento'
+import "./EscalaAberta.css";
+import MusicoInstrumento from "./MusicoInstrumento";
 function EscalaAberta() {
-    return (
-        <div className="containeri">
-            <div className="header">
-                <h2>Nome da Escala</h2>
-            </div>
+  return (
+    <div className="containeri">
+      <div className="header">
+        <h2>Nome da Escala</h2>
+      </div>
 
+      <div className="corpo">
+        <MusicoInstrumento />
+        <MusicoInstrumento />
+        <MusicoInstrumento />
+        <MusicoInstrumento />
+      </div>
 
-            <div className='corpo'>
-                <MusicoInstrumento />
-                <MusicoInstrumento />
-                <MusicoInstrumento />
-                <MusicoInstrumento />
-            </div>
+      <div className="corpo-musicas">
+        <h5>Musica 1 - autor</h5>
+        <h5>Musica 2 - autor</h5>
+        <h5>Musica 3 - autor</h5>
+        <h5>Musica 4 - autor</h5>
+      </div>
+     
+            
 
-            <div className='corpo-musicas'>
-
-            </div>
-
-
-            <div className='footer'>
-
-            </div>
+      <div className="footer-escala-aberta">
+        <div className="date-ftr">
+          <h2>Data</h2>
+          <p>27/11/2022</p>
         </div>
-    )
+        <div className="local-ftr">
+          <h2>Local</h2>
+          <p>Igreja</p>
+        </div>
+        <div className="hora-ftr">
+          <h2>Horario</h2>
+          <p> 17:30</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default EscalaAberta;
